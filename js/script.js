4601d9e33a2b832dbe7c84da4ff4623bcb5184e0
fares elabasery <81824063+FaresElabasery@users.noreply.github.com>
2024-10-29 05:41:56 +0300

    fix : fix first image in card

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ function changeIcon(obj) {
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-3 rounded-circle m-1"></i>
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-4 m-1"></i>
         `
-        imgElement.innerHTML = `  <img class="img-fluid" src="images/popular-1.svg" alt="planner">`
+        imgElement.innerHTML = `<img class="img-fluid" src="images/popular-1.svg" alt="planner">`
     } else if (obj.classList.contains('color-2')) {
         // Perform actions when obj has class name color-1
         circleElement.style.backgroundColor = 'var(--purple-color)';
@@ -26,7 +26,7 @@ function changeIcon(obj) {
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-3 rounded-circle m-1"></i>
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-4 m-1"></i>
         `
-        imgElement.innerHTML = `  <img class="img-fluid" src="images/popular-2.svg" alt="planner">`
+        imgElement.innerHTML = `<img class="img-fluid" src="images/popular-2.svg" alt="planner">`
     } else if (obj.classList.contains('color-3')) {
         // Perform actions when obj has class name color-1
         circleElement.style.backgroundColor = 'var(--white-color)';
@@ -36,7 +36,7 @@ function changeIcon(obj) {
         <i onclick="changeIcon(this)" class="fa-regular fa-circle rounded-circle hover-color-3 m-1"></i>
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-4 m-1"></i>
         `
-        imgElement.innerHTML = `  <img class="img-fluid" src="images/popular-4.svg" alt="planner">`
+        imgElement.innerHTML = `<img class="img-fluid" src="images/popular-4.svg" alt="planner">`
     } else if (obj.classList.contains('color-4')) {
         // Perform actions when obj has class name color-1
         circleElement.style.backgroundColor = 'var(--green-color)';
@@ -47,7 +47,7 @@ function changeIcon(obj) {
         <i onclick="changeIcon(this)"class="fa-solid fa-circle color-3 m-1"></i>
         <i onclick="changeIcon(this)" class="fa-regular fa-circle rounded-circle hover-color-4 m-1"></i>
         `
-        imgElement.innerHTML = `  <img class="img-fluid" src="images/popular-3.svg" alt="planner">`
+        imgElement.innerHTML = `<img class="img-fluid" src="images/popular-3.svg" alt="planner">`
     } else {
         console.log('cant change icon');
     }
